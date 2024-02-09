134e211276303f2919d56b2d59aa97c32ffb98ac
Abusik019 <abusroyale@gmail.com>
2024-02-09 12:57:03 +0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import cat from "./assets/cat.jpg";
+
+function App() {
+
+  return (
+    <>
+    <Header />
+      <div>hello</div>
+      <img src={cat} />
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <>
+      <ul>
+        <li>Main</li>
+        <li>About us</li>
+        <li>Service</li>
+        <li className="contacts">Contacts</li>
+      </ul>
+    </>
+  )
+}
